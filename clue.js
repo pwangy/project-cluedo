7f405f116a40618feae137fb2765ce2f84c5a0a3
Peggy <64912576+pwangy@users.noreply.github.com>
2020-09-15 15:08:14 +0200

    populated js with suspects, weapons, and rooms.

--- clue.js
+++ clue.js
@@ -1,35 +1,130 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
-
 const mrGreen = {
-  firstName: 'Jacob',
+  firstName: 'John',
   lastName: 'Green',
   color: 'green',
-  description: 'He has a lot of connections',
-  age: 45,
+  description: 'A defrocked priest turned tycoon, he does not like people looking into his past.',
+  age: 53,
   image: 'assets/green.png',
-  occupation: 'Entrepreneur'
-}
+  occupation: 'Businessman',
+  favoriteWeapon: 'lead pipe'
+};
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
+const profPlum = {
+  firstName: 'Peter',
+  lastName: 'Plum',
+  color: 'purple',
+  description: 'An archaeologist with a dubious reputation, he has worked on a number of mysterious cases.',
+  age: 37,
+  image: 'assets/plum.png',
+  occupation: 'Archeologist',
+  favoriteWeapon: 'poison'
+};
+
+const missScarlett = {
+  firstName: 'Josephine',
+  lastName: 'Scarlett',
+  color: 'red',
+  description: 'Red has always been the colour of danger...',
+  age: 35,
+  image: 'assets/scarlet.png',
+  occupation: 'Madam',
+  favoriteWeapon: 'rope'
+};
+
+const mrsPeacock = {
+  firstName: 'Patricia',
+  lastName: 'Peacock',
+  color: 'blue',
+  description: 'An ornithologist with a special interest in birds of prey...',
+  age: 48,
+  image: 'assets/peacock.png',
+  occupation: 'Ornithologist',
+  favoriteWeapon: 'hatPin'
+};
+
+const colMustard = {
+  firstName: 'Michael',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'A distinguished soldier and hero of many battles, he is also an excellent shot.',
+  age: 61,
+  image: 'assets/mustard.png',
+  occupation: 'Military',
+  favoriteWeapon: 'wrench'
+};
+
+const mrsWhite = {
+  firstName: 'Blanche',
+  lastName: 'White',
+  color: 'white',
+  description: 'Was she looking through the keyhole at the very moment the crime took place?',
+  age: 62,
+  image: 'assets/peacock.png',
+  occupation: 'Cook',
+  favoriteWeapon: 'candlestick'
+};
 
 const rope = {
-  name: 'rope',
-  weight: 10
-}
+  name: 'Rope',
+  weight: 10,
+  id: 'rope',
+  description: 'Who could have tied such a strong knot?'
+};
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+const knife = {
+  name: 'Knife',
+  weight: 10,
+  id: 'knife',
+  description: 'The steel is shining as though it was recently cleaned...'
+};
 
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
+const candlestick = {
+  name: 'Candlestick',
+  id: 'candlestick',
+  description: 'Very solid brass and very, very blunt.'
+};
 
-const suspects = [
-  mrGreen,
-  mrsWhite
-  // ...  and the rest
-]
+const leadPipe = {
+  name: 'Lead Pipe',
+  id: 'lead pipe',
+  description: 'Who could have struck such a blow?'
+};
 
-const weapons = []
+const poison = {
+  name: 'Poison',
+  id: 'poison',
+  description: 'Who broke the wax seal on the vial?'
+};
 
-const rooms = []
+const horseshoe = {
+  name: 'Horseshoe',
+  id: 'horseshoe',
+  description: 'What suspect brings his own horses to the country?'
+};
+
+const hatPin = {
+  name: 'Hat Pin',
+  id: 'hatPin',
+  description: 'Small, sharp, and discreet.'
+};
+
+const wrench = {
+  name: 'Wrench',
+  id: 'wrench',
+  description: 'Whose fingerprints are on the grip?'
+};
+
+const revolver = {
+  name: 'Revolver',
+  id: 'revolver',
+  description: 'The mechanism seems well-oiled...'
+};
+
+const suspects = [mrGreen, profPlum, missScarlett, mrsPeacock, colMustard, mrsWhite];
+
+const weapons = [rope, knife, candlestick, leadPipe, poison, horseshoe, hatPin, wrench, revolver];
+
+const rooms = [Gazebo, Conservatory,  Kitchen, Study, Library, Fountain, Lounge, Ballroom, Hall, Spa, Stables, Observatory, Theater, Garage, Patio];
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
@@ -41,17 +136,20 @@ const randomSelector = array => {
 // With a killer, a weapon and a room.
 // The values will be set later.
 
+const mystery = `${suspects} ${weapons} ${rooms}`;
+
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
+}
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById('killerCard').style.background = mystery.killer.color
+  
   document.getElementById(
     'killerName'
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
-}
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
